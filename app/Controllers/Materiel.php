a69c86fcfe9f6f162d7294d1f504e85a7caeff9c
Lin Marc <marclin750@gmail.com>
2024-04-20 17:05:04 +0200

    commit all

--- app/Controllers/Materiel.php
+++ app/Controllers/Materiel.php
@@ -31,21 +31,17 @@ class Materiel extends BaseController
         $data['materiels'] = [];
         $data['nbr_materiel_type1'] = 0;
         
-
         $materiel1 = $this->materielModel->where('categorie', 'informatique')->where('reserver', 0)->findAll();
         $data['materiels']['categorie'] = $materiel1;
         $data['nbr_materiel_type1'] = count($materiel1);
 
-        
         $materiel2 = $this->materielModel->where('categorie', 'imprimante')->where('reserver', 0)->findAll();
         $data['materiels']['imprimante'] = $materiel2;
         $data['nbr_materiel_type2'] = count($materiel2);
-
         
         $materiel3 = $this->materielModel->where('categorie', 'internet')->where('reserver', 0)->findAll();
         $data['materiels']['internet'] = $materiel3;
         $data['nbr_materiel_type3'] = count($materiel3);
-
         
         $materiel4 = $this->materielModel->where('categorie', 'video')->where('reserver', 0)->findAll();
         $data['materiels']['video'] = $materiel4;
@@ -62,7 +58,6 @@ class Materiel extends BaseController
         $materiel7 = $this->materielModel->where('categorie', 'transmission')->where('reserver', 0)->findAll();
         $data['materiels']['transmission'] = $materiel7;
         $data['nbr_materiel_type7'] = count($materiel7);
-
 
         $materiel = view('pages/materiel',$data);
         return $this->header . $this->navbar . $materiel . $this->footer;
@@ -81,7 +76,6 @@ class Materiel extends BaseController
         $materiel2 = $this->materielModel->where('categorie', 'imprimante')->where('reserver', 0)->findAll();
         $data['materiels'] = $materiel2;
         $data['nbr_materiel_type2'] = count($materiel2);
-
     
         $type2 = view('pages/materiel/type2', $data);
         return $this->header . $this->navbar . $type2 . $this->footer;
@@ -103,37 +97,30 @@ class Materiel extends BaseController
 
         $type4 = view('pages/materiel/type4', $data);
         return $this->header . $this->navbar . $type4 . $this->footer;
-
     }
 
-
     public function type5() : string{
-        
         $materiel5 = $this->materielModel->where('categorie', 'photocopieur')->where('reserver', 0)->findAll();
         $data['materiels'] = $materiel5;
         $data['nbr_materiel_type5'] = count($materiel5);
 
         $type5 = view('pages/materiel/type5', $data);
         return $this->header . $this->navbar . $type5 . $this->footer;
-
     }
 
     public function type6() : string{
-        
         $materiel6 = $this->materielModel->where('categorie', 'accessoire')->where('reserver', 0)->findAll();
         $data['materiels'] = $materiel6;
         $data['nbr_materiel_type6'] = count($materiel6);
 
         $type6 = view('pages/materiel/type6', $data);
         return $this->header . $this->navbar . $type6 . $this->footer;
-
     }
 
     public function type7() : string{
         $materiel7 = $this->materielModel->where('categorie', 'transmission')->where('reserver', 0)->findAll();
         $data['materiels'] = $materiel7;
         $data['nbr_materiel_type7'] = count($materiel7);
-
         
         $type7 = view('pages/materiel/type7', $data);
         return $this->header . $this->navbar . $type7 . $this->footer;
