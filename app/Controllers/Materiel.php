1640d9abb9d890baa6597afee2e240fad104df4c
giangkevin <149978467+giangkevin@users.noreply.github.com>
2024-04-20 03:59:59 +0200

    Add type 1 function

--- app/Controllers/Materiel.php
+++ app/Controllers/Materiel.php
@@ -3,12 +3,49 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\MaterielModel;
+use App\Models\ReservationMaterielModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Materiel extends BaseController
 {
-    public function index()
+    protected $header;
+    protected $navbar;
+    protected $footer;
+
+    protected $materielModel;
+    protected $reservationMaterielModel;
+
+    public function __construct()
     {
-        //
+        $this->header = view('template/header');
+        $this->navbar = view('components/navbar');
+        $this->footer = view('template/footer');
+
+        $this->materielModel = new MaterielModel();
+        $this->reservationMaterielModel = new ReservationMaterielModel();
+    }
+    public function view()
+    {
+
+        $data['materiels'] = [];
+        $data['nbr_materiel_type1'] = 0;
+        
+
+        $materiel1 = $this->materielModel->where('categorie', 'informatique')->where('reserver', 0)->findAll();
+        $data['materiels']['categorie'] = $materiel1;
+        $data['nbr_materiel_type1'] = count($materiel1);
+
+        $materiel = view('pages/materiel');
+        return $this->header . $this->navbar . $materiel . $this->footer;
+    }
+
+    public function type1(): string{
+        $materiel1 = $this->materielModel->where('categorie', 'informatique')->where('reserver', 0)->findAll();
+        $data['materiels'] = $materiel1; // Supprimez le niveau de l'indice 'informatique'
+        $data['nbr_materiel_type1'] = count($materiel1);
+    
+        $type1 = view('pages/materiel/type1', $data);
+        return $this->header . $this->navbar . $type1 . $this->footer;
     }
 }
