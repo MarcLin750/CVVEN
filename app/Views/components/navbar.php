bcca57e242f4e2aa8d090e3eef6a995092165c49
Lin Marc <marclin750@gmail.com>
2024-04-21 02:43:46 +0200

    login styles modify

--- app/Views/components/navbar.php
+++ app/Views/components/navbar.php
@@ -16,13 +16,13 @@
                     <a class="nav-link me-2" href="/materiel/">Matériel</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link me-2" href="/">Salle de colloque</a>
+                    <a class="nav-link me-2" href="#">Salle de colloque</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link me-2" href="/">Contact</a>
+                    <a class="nav-link me-2" href="#">Contact</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link me-2" href="/">À propos</a>
+                    <a class="nav-link me-2" href="#">À propos</a>
                 </li>
             </ul>
             <div class="d-flex">
